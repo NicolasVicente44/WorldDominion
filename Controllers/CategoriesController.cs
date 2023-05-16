bf31a4178d7b6747486e84ea6590a37c691622a8
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-05-16 16:45:35 -0400

    testing routes

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorldDominion.Models;
+
+namespace WorldDominion.Controllers
+{
+    public class CategoriesController : Controller
+    {
+
+        private List<Category> Categories { get; set; }
+
+
+
+        public CategoriesController()
+        {
+            Categories = new List<Category>()
+            {
+                new Category { Id = 1, Name = "Dairy and Eggs", Description = "Find all the best milks and ovums" },
+                new Category { Id = 2, Name = "Fruits and Vegtables", Description = "Lettuce turn up the beet! It's kind of a big dill." },
+                new Category { Id = 3, Name = "Meats", Description = "Were a provider of a fine meats." }
+            };
+
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View(Categories);
+        }
+
+
+        public IActionResult Browse(int id)
+        {
+            {
+                var category = Categories.Find(category => category.Id == id);
+                return View(category);
+            }
+        }
+    }
+}
---
+++ Models/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿namespace WorldDominion.Models
+{
+    public class MenuItem
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Label { get; set; }
+
+
+
+    }
+}
