1937455fc32d0bea3064fb6ec5773a842b77bcd4
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-05-30 17:07:48 -0400

    Added azure database connection via ms server and azure portal. add-migration (migration name)  completed and update-database completed

--- Components/ViewComponents/NavigationMenuViewComponent.cs
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -12,9 +12,13 @@ namespace WorldDominion.Components.ViewComponents
             {
                     new MenuItem {Controller = "Home", Action = "Index", Label = "Home"},
                     new MenuItem {Controller = "Home", Action = "About", Label = "About"},
-                    new MenuItem {Controller = "Categories", Action = "Index", Label = "Categories", DropdownItems = new List<MenuItem> {
-                           new MenuItem {Controller = "Categories", Action = "Index", Label = "List"}, 
-                           new MenuItem {Controller = "Categories", Action = "Create", Label = "Create"}, 
+                    new MenuItem {Controller = "Departments", Action = "Index", Label = "Departments", DropdownItems = new List<MenuItem> {
+                    new MenuItem {Controller = "Departments", Action = "Index", Label = "List"}, 
+                    new MenuItem {Controller = "Departments", Action = "Create", Label = "Create"}, 
+                    } },
+                    new MenuItem {Controller = "Products", Action = "Index", Label = "Products", DropdownItems = new List<MenuItem> {
+                    new MenuItem {Controller = "Products", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "Products", Action = "Create", Label = "Create"},
                     } },
                     new MenuItem {Controller = "Home", Action = "Privacy", Label = "Privacy"}
 
---
+++ Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace WorldDominion.Models
+{
+    public enum ProductWeightUnit
+    {
+        Grams,
+        Kilograms,
+        Pounds,
+        Ounces,
+        Litres
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required()]
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+
+        [Required()]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string? Description { get; set; }
+
+        [Required(), Range(0.01, 9999999.99)]
+        public double MSRP { get; set; }
+
+        [Required()]
+        [Column(TypeName = "decimal(18, 2)")]
+
+        public decimal Weight { get; set; }
+
+        [Required()]
+        public ProductWeightUnit WeightUnit { get; set; }
+
+        public string? Photo { get; set; }
+        public Department? Department { get; set; }
+    }
+}
